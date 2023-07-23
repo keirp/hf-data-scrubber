3ab3289c5b754c82ba77b608e91c276b2c100597
Keiran Paster <keiran@me.com>
2023-07-22 17:11:46 -0700

    initial commit

---
+++ create_test_set.py
@@ -0,0 +1,147 @@
+"""Create a test set from popular LLM evaluation datasets."""
+import argparse
+import inspect
+from datasets import load_dataset
+import jsonlines
+import custom_datasets.hendrycks_math.hendrycks_math
+import glob
+
+MMLU_SUBJECTS = [
+    "abstract_algebra",
+    "anatomy",
+    "astronomy",
+    "business_ethics",
+    "clinical_knowledge",
+    "college_biology",
+    "college_chemistry",
+    "college_computer_science",
+    "college_mathematics",
+    "college_medicine",
+    "college_physics",
+    "computer_security",
+    "conceptual_physics",
+    "econometrics",
+    "electrical_engineering",
+    "elementary_mathematics",
+    "formal_logic",
+    "global_facts",
+    "high_school_biology",
+    "high_school_chemistry",
+    "high_school_computer_science",
+    "high_school_european_history",
+    "high_school_geography",
+    "high_school_government_and_politics",
+    "high_school_macroeconomics",
+    "high_school_mathematics",
+    "high_school_microeconomics",
+    "high_school_physics",
+    "high_school_psychology",
+    "high_school_statistics",
+    "high_school_us_history",
+    "high_school_world_history",
+    "human_aging",
+    "human_sexuality",
+    "international_law",
+    "jurisprudence",
+    "logical_fallacies",
+    "machine_learning",
+    "management",
+    "marketing",
+    "medical_genetics",
+    "miscellaneous",
+    "moral_disputes",
+    "moral_scenarios",
+    "nutrition",
+    "philosophy",
+    "prehistory",
+    "professional_accounting",
+    "professional_law",
+    "professional_medicine",
+    "professional_psychology",
+    "public_relations",
+    "security_studies",
+    "sociology",
+    "us_foreign_policy",
+    "virology",
+    "world_religions",
+]
+
+MATH_SUBJECTS = [
+    'algebra', 
+    'counting_and_probability', 
+    'geometry', 
+    'intermediate_algebra', 
+    'number_theory', 
+    'prealgebra', 
+    'precalculus'
+]
+
+def get_texts(path):
+    # Use glob
+    texts = []
+    for file in glob.glob(path):
+        with open(file, 'r') as f:
+            texts.append(f.read())
+    return texts
+
+def format_mmlu(doc):
+    """We only deduplicate against the question text since finding the choices in the exact order
+    seems unlikely."""
+    question = doc["question"].strip()
+    return question
+
+def add_texts_to_output(texts, output):
+    with jsonlines.open(output, mode='a') as writer:
+        for txt in texts:
+            writer.write(txt)
+
+def main(args):
+    # Remove existing test set
+    with jsonlines.open(args.output, mode='w') as writer:
+        writer.write_all([])
+
+    # Add gsm8k test set
+    ds = load_dataset('gsm8k', 'main', split='test')
+    add_texts_to_output([obj['answer'] for obj in ds], args.output)
+
+    # Add MMLU test set
+    for subject in MMLU_SUBJECTS:
+        ds = load_dataset('cais/mmlu', subject, split='test')
+        add_texts_to_output([format_mmlu(obj) for obj in ds], args.output)
+
+    # Add Hendrycks MATH test set
+    for subject in MATH_SUBJECTS:
+        ds = load_dataset(inspect.getfile(custom_datasets.hendrycks_math.hendrycks_math), subject, split='test')
+        # Add all problems
+        add_texts_to_output([obj['problem'] for obj in ds], args.output)
+        # Add all solutions
+        add_texts_to_output([obj['solution'] for obj in ds], args.output)
+
+    # Add ProofNet
+    ds = load_dataset('hoskinson-center/proofnet', split='test')
+    # Add nl_statement
+    add_texts_to_output([obj['nl_statement'] for obj in ds], args.output)
+    # Add nl_proof
+    add_texts_to_output([obj['nl_proof'] for obj in ds], args.output)
+    # Add formal_statement
+    add_texts_to_output([obj['formal_statement'] for obj in ds], args.output)
+
+    # Add miniF2F
+    # Add hollight
+    texts = get_texts('miniF2F/hollight/test/*.ml')
+    add_texts_to_output(texts, args.output)
+    # Add Isabelle
+    texts = get_texts('miniF2F/isabelle/test/*.thy')
+    add_texts_to_output(texts, args.output)
+    # Add Lean
+    texts = get_texts('miniF2F/lean/src/test.lean')
+    add_texts_to_output(texts, args.output)
+    # Add MetaMath
+    texts = get_texts('miniF2F/metamath/test/*.mm')
+    add_texts_to_output(texts, args.output)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', type=str, default='data/test.jsonl')
+    args = parser.parse_args()
+    main(args)
---
+++ hf_data_scrubber.py
@@ -0,0 +1,69 @@
+import argparse
+from datasets import load_dataset
+from nltk.util import ngrams
+import jsonlines
+from tqdm import tqdm
+import re
+
+def split_on_contamination(data_batch, test_ngrams, args):
+    # Data batch comes as a dict with list values. Let's reorganize it as a list of dicts
+    # data_batch = [{k: v[i] for k, v in data_batch.items()} for i in range(len(data_batch[args.column]))]
+    # Compute 13-grams for the batch
+    ngrams_batch = []
+    for txt in data_batch[args.column]:
+        obj_ngrams = ngrams(txt.split(), args.n)
+        obj_ngrams = [' '.join(ngram) for ngram in obj_ngrams]
+        ngrams_batch.append(set(obj_ngrams))
+    
+    decontaminated_batch = {k: [] for k in data_batch.keys()}
+
+    # Compare the ngrams of the batch with the ngrams of the test set
+    for i in range(len(data_batch)):
+        if ngrams_batch[i].intersection(test_ngrams):
+            # print('Found contamination in text: ', data_batch[args.column][i])
+            print('Contaminating ngrams: ', ngrams_batch[i].intersection(test_ngrams))
+            # Split the text on the ngrams in the intersection
+            # First, create a regex to split on the ngrams
+            regex = '|'.join(re.escape(ngram) for ngram in ngrams_batch[i].intersection(test_ngrams))
+            # Split the text
+            split_text = re.split(regex, data_batch[args.column][i])
+            # print('Split text: ', split_text)
+            # Now for each of these splits, add a new item to the batch
+            for txt in split_text:
+                decontaminated_batch[args.column].append(txt)
+                for k in data_batch.keys():
+                    if k != args.column:
+                        decontaminated_batch[k].append(data_batch[k][i])
+        else:
+            for k in data_batch.keys():
+                decontaminated_batch[k].append(data_batch[k][i])
+
+    return decontaminated_batch  
+
+def main(args):
+    ds = load_dataset(args.dataset)
+    # Load test_set
+    with jsonlines.open(args.test_set) as reader:
+        test_set = [obj for obj in reader]
+
+    # Compute 13-grams for the test set
+    print('Computing 13-grams for the test set')
+    test_ngrams = []
+    for obj in tqdm(test_set):
+        obj_ngrams = ngrams(obj.split(), args.n)
+        obj_ngrams = [' '.join(ngram) for ngram in obj_ngrams]
+        test_ngrams.extend(list(obj_ngrams))
+    test_ngrams = set(test_ngrams)
+
+    decontaminated_dataset = ds.map(lambda x: split_on_contamination(x, test_ngrams, args), batched=True, batch_size=1000, num_proc=args.n_processes)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, default='keirp/open-web-math-hq-dev')
+    parser.add_argument('--column', type=str, default='text')
+    parser.add_argument('--test_set', type=str, default='data/test.jsonl')
+    parser.add_argument('--n', type=int, default=13)
+    parser.add_argument('--n_processes', type=int, default=8)
+
+    args = parser.parse_args()
+    main(args)
